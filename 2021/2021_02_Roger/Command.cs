04d60f33ba3562b30c72cfa7e4bc1bddf9063103
rdunder <r.dunder@gmail.com>
2023-11-26 15:58:20 +0100

    Added project 2021_02_Roger

---
+++ 2021/2021_02_Roger/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_02_Roger
+{
+    internal class Command
+    {
+        public string Cmd { get; set; }
+        public int CmdValue { get; set; }
+
+        public Command(string command, int commandValue) 
+        {
+            Cmd = command;
+            CmdValue = commandValue;
+        }
+    }
+}
---
+++ 2021/2021_02_Roger/Program.cs
@@ -0,0 +1,44 @@
+﻿using _2021_02_Roger;
+using System.Collections.Generic;
+
+string pathToInput = "../../../input.txt";
+var commands = GetCommands(File.ReadAllLines(pathToInput));
+
+
+int horizontalPosition = 0;
+int depth = 0;
+
+foreach (var command in commands)
+{
+    if (command.Cmd == "forward") { horizontalPosition += command.CmdValue; }
+    if (command.Cmd == "down") {  depth += command.CmdValue; }
+    if (command.Cmd == "up") { depth -= command.CmdValue; }
+}
+
+Console.WriteLine($"Horizontal Position is now: {horizontalPosition} and the Depth is: {depth}");
+Console.WriteLine($"The answer ro the problem is {horizontalPosition * depth}");
+
+
+List<Command> GetCommands(string[] inputArray)
+{
+    List<Command> commands = new List<Command>();
+
+    foreach (var line in inputArray)
+    {
+        string[] inputLineArray = line.Split(" ");
+        string command = inputLineArray[0];
+
+        if (Int32.TryParse(inputLineArray[1], out int commandValue))
+        {
+            commands.Add( new Command(command, commandValue) );
+        }
+        else
+        {
+            Console.WriteLine("something worng with the parser");
+            Console.ReadKey();
+            break;
+        }
+    }
+
+    return commands;
+}
