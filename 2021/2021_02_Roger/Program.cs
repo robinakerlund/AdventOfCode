1c543464a25a68669f52a7e49bbace7582d35dfd
rdunder <r.dunder@gmail.com>
2023-11-26 18:19:36 +0100

    2021_02_Roger Done

--- 2021/2021_02_Roger/Program.cs
+++ 2021/2021_02_Roger/Program.cs
@@ -5,14 +5,48 @@ string pathToInput = "../../../input.txt";
 var commands = GetCommands(File.ReadAllLines(pathToInput));
 
 
+
+//  Part 1
+//
+//int horizontalPosition = 0;
+//int depth = 0;
+
+//foreach (var command in commands)
+//{
+//    if (command.Cmd == "forward") { horizontalPosition += command.CmdValue; }
+//    if (command.Cmd == "down") {  depth += command.CmdValue; }
+//    if (command.Cmd == "up") { depth -= command.CmdValue; }
+//}
+
+//Console.WriteLine($"Horizontal Position is now: {horizontalPosition} and the Depth is: {depth}");
+//Console.WriteLine($"The answer ro the problem is {horizontalPosition * depth}");
+
+
+
+//  Part 2
+//
 int horizontalPosition = 0;
 int depth = 0;
+int aim = 0;
 
 foreach (var command in commands)
 {
-    if (command.Cmd == "forward") { horizontalPosition += command.CmdValue; }
-    if (command.Cmd == "down") {  depth += command.CmdValue; }
-    if (command.Cmd == "up") { depth -= command.CmdValue; }
+
+    if (command.Cmd == "forward")
+    {
+        horizontalPosition += command.CmdValue;
+        depth += command.CmdValue * aim;
+    }
+
+    if (command.Cmd == "down")
+    {
+        aim += command.CmdValue;
+    }
+
+    if (command.Cmd == "up")
+    {
+        aim -= command.CmdValue;
+    }
 }
 
 Console.WriteLine($"Horizontal Position is now: {horizontalPosition} and the Depth is: {depth}");
