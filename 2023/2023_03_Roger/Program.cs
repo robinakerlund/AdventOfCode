caa4edb41f39103e10289f994af5eeb77d782c96
rdunder <r.dunder@gmail.com>
2023-12-04 23:55:50 +0100

    2023_03_Roger: Part 1 is Done
    2023_04_Roger: Part 1 is done.

---
+++ 2023/2023_03_Roger/Program.cs
@@ -0,0 +1,127 @@
+﻿
+// 533421 is too low
+
+string[] input = File.ReadAllLines("../../../input.txt");
+
+Console.WriteLine($"Ammount of lines: {input.Length} - Chars in each: {input[0].Length}");
+
+string[] exampleInput =
+{
+"..#.....",
+".......2",
+"1.....%."
+};
+
+//string[,] schematic = Get2dArray(exampleInput);
+string[,] schematic = Get2dArray(input);
+List<string> parts = new();
+
+for (int row = 0; row < schematic.GetLength(0); row++)
+{
+    string part = string.Empty;
+    bool isPart = false;
+
+    for (int col = 0; col < schematic.GetLength(1); col++)
+    {
+        if (Int32.TryParse(schematic[row, col], out int singleDigit))
+        {
+            part += singleDigit.ToString();
+
+            if (CheckIf_IsPart(schematic, row, col))
+            {
+                isPart = true;
+            }
+
+        }
+        else
+        {         
+            if (isPart)
+            {
+                parts.Add(part);
+                isPart = false;
+            }
+            
+            part = string.Empty;
+            
+        }
+
+    }
+    
+}
+
+int sum = 0;
+foreach (string part in parts)
+{
+    sum += Convert.ToInt32(part);
+    Console.WriteLine(Convert.ToInt32(part));
+}
+
+Console.WriteLine(sum);
+
+string[,] Get2dArray(string[] input)
+{
+    string[,] output = new string[input.Length, input[0].Length + 1];
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        for (int j = 0; j < input[i].Length; j++)
+        {
+            output[i, j] = input[i][j].ToString();
+        }
+        output[i, input[i].Length] = ".";
+    }
+
+    return output;
+}
+bool CheckIf_IsPart(string[,] arrayToCheck, int row, int col)
+{
+    string[] symbolsToIgnore = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."};
+    //string[] symbolsToIgnore = { "." };
+
+    if (col > 0)
+    {
+        if (!symbolsToIgnore.Contains(arrayToCheck[row, col - 1]))
+        {            
+            return true;
+        }
+    }
+    if (col < arrayToCheck.GetLength(1) - 1)
+    {        
+        if (!symbolsToIgnore.Contains(arrayToCheck[row, col + 1]))
+        {
+            return true;
+        }
+    }
+    if (row > 0)
+    {
+        if (!symbolsToIgnore.Contains(arrayToCheck[row - 1, col]))
+        { 
+            return true; 
+        }
+        if (col > 0 && !symbolsToIgnore.Contains(arrayToCheck[row - 1, col - 1]))
+        {
+            return true;
+        }
+        if (col < arrayToCheck.GetLength(1) - 1 && !symbolsToIgnore.Contains(arrayToCheck[row - 1, col + 1]))
+        {
+            return true;
+        }
+    }
+    if (row < arrayToCheck.GetLength(0) - 1)
+    {
+        if (!symbolsToIgnore.Contains(arrayToCheck[row + 1, col]))
+        {
+            return true;
+        }
+        if (col > 0 && !symbolsToIgnore.Contains(arrayToCheck[row + 1, col - 1]))
+        {
+            return true;
+        }
+        if (col < arrayToCheck.GetLength(1) - 1 && !symbolsToIgnore.Contains(arrayToCheck[row + 1, col + 1]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
---
+++ 2023/2023_04_Roger/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] input = File.ReadAllLines("../../../input.txt");
+
+string[] exampleInput =
+{
+    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+};
+
+int sumOfWinnings = 0;
+
+foreach (string card in input)
+{
+    List<int> myNumbers = ConvertStringArrayToIntArray( card.Split(':')[1].Split('|')[0].Split(" ") );
+    List<int> drawnNumbers = ConvertStringArrayToIntArray( card.Split(":")[1].Split("|")[1].Split(" ") );
+    int AmmountOfWinnings = 0;
+
+    foreach (int number in myNumbers )
+    {
+        if (drawnNumbers.Contains(number))
+        {
+            AmmountOfWinnings++;
+        }
+    }
+    sumOfWinnings += Convert.ToInt32( Math.Pow(2, AmmountOfWinnings - 1) );
+
+    Console.WriteLine("amount of winnings: " + AmmountOfWinnings);
+    Console.WriteLine("sum atm: " + sumOfWinnings);
+}
+
+Console.WriteLine(sumOfWinnings);
+
+List<int> ConvertStringArrayToIntArray(string[] input)
+{
+    List<int> output = new();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (Int32.TryParse(input[i], out int digit))
+        {
+            output.Add(digit);
+        }
+    }
+
+    return output;
+}
